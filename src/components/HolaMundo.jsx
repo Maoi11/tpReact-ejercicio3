ed2896ea8a7dc3c744f2af4ab5bbe498c3466736
Maoi11 <marisaindianagarat@gmail.com>
2023-05-29 01:15:57 -0300

    se da formato y se corrige error en importacion de boton bootstrap

--- src/components/HolaMundo.jsx
+++ src/components/HolaMundo.jsx
@@ -1,13 +1,19 @@
 import { useState } from "react";
+import { Button } from "react-bootstrap";
 
-const HolaMundo = ({nuevoMensaje}) => {
-    const [msj, setMsj] = useState(" ")
-    return (
-        <div className="text-center">
-            <h1>Hello {nuevoMensaje}{msj}!</h1>
-            <button variant="success" onClick={()=>setMsj("(from changed state)")}>Click here to see changed State!/</button>
-        </div>
-    );
+const HolaMundo = ({ nuevoMensaje }) => {
+  const [msj, setMsj] = useState(" ");
+  return (
+    <div className="text-center">
+      <h1>
+        Hello {nuevoMensaje}
+        {msj}!
+      </h1>
+      <Button variant="success" onClick={() => setMsj("(from changed state)")}>
+        Click here to see changed State!
+      </Button>
+    </div>
+  );
 };
 
 export default HolaMundo;
