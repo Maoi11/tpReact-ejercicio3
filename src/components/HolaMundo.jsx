fb0aef52161e0d6bcda3f578722628aefb2d0998
Maoi11 <marisaindianagarat@gmail.com>
2023-05-29 00:59:16 -0300

    se agrega state, boton para cambiar state

--- src/components/HolaMundo.jsx
+++ src/components/HolaMundo.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
+
 const HolaMundo = ({nuevoMensaje}) => {
+    const [msj, setMsj] = useState(" ")
     return (
-        <div>
-            <h1>Hello {nuevoMensaje}!</h1>
+        <div className="text-center">
+            <h1>Hello {nuevoMensaje}{msj}!</h1>
+            <button variant="success" onClick={()=>setMsj("(from changed state)")}>Click here to see changed State!/</button>
         </div>
     );
 };
